09f76bf1aad8a9324bde09fec46f0de8795e75db
Himanshu Chauhan <hchauhan@aistrike.com>
2024-07-15 21:19:53 +0530

    feat: Ui updates for Chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Input from "@/components/common/Input";
-
+import Image from "next/image";
+import { CgAttachment } from "react-icons/cg";
 
 export default function Home() {
   return (
 
-    <main className="flex min-h-screen bg-gray-700 flex-col items-center justify-between ">
-      
-      <div>
+    <main className="  bg-[#333333]  ">
+      <div className=" max-w-5xl mx-auto w-full min-h-screen flex flex-col justify-between ">
+        
+        <div className="h-2/3 flex-grow">
+          
+        </div>
 
-      </div>
+        <div className="w-full  mb-4 flex items-center relative text-white ">
+          <CgAttachment size={20} className="absolute left-4  "/>
+          <Input type="text" className=" bg-[#4D4D4D] border-none focus:outline-none pl-12 py-3  placeholder:text-[#ececec] text-[#ececec]" />
+        </div>
 
-      <div className="w-full ">
-        <Input type="text" className=" border-black bg-transparent focus:ring-0 placeholder:text-white"  />
       </div>
     </main>
   );
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,6 +3,6 @@ import { FC, InputHTMLAttributes } from 'react'
 type P = {} & InputHTMLAttributes<HTMLInputElement>
 
 const Input:FC<P> =({ className , ...rest })=>{
-  return <input {...rest} placeholder='Enter your query' className={' px-4 py-2 rounded-sm w-full '+className} />
+  return <input {...rest} placeholder='Enter your query' className={' rounded-3xl w-full '+className} />
 }
 export default Input;
